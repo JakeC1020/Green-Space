9f4311f3cdfc1ccac0f26cd94b44523475322440
Jake Crawford <jake.crawford@outlook.com>
2015-10-10 20:31:54 +0000

    Add session package and base code

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,10 @@ module.exports = function(db) {
   var verbs = {
     get: function(req, res) {
       res.render('pages/register');
+    },
+    post: function(req, res) {
+      req.session.loggedIn = true;
+      res.redirect('/');
     }
   };
   
