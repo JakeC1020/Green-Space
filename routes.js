d57a0da7e057ad96a7914937febe9097b00937a6
Evan Su <hexacyanide@gmail.com>
2015-10-10 17:42:48 +0000

    Expose database object to routes.

--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
 var routes = fs.readdirSync('./routes');
+var database = require('./lib/database');
 
 module.exports = function(app) {
   routes.forEach(function(entry) {
-    var route = require('./routes/' + entry);
+    var route = require('./routes/' + entry)(database);
     
     for (var verb in route.verbs) {
+      console.log('Loaded route: ' + verb.toUpperCase() + ' ' + route.route);
       app[verb](route.route, route.verbs[verb]);
     }
   });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-module.exports = {
-  route: '/',
-  verbs: {
+module.exports = function(db) {
+  var verbs = {
     get: function(req, res) {
       res.sendStatus(200);
     }
-  }
+  };
+  
+  return { route: '/', verbs: verbs };
 };
